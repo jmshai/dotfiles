075a05132f6248f59917036ac0953a1d9390f4ff
shai <jmshailesh587@gmail.com>
2026-05-24 02:11:16 +0800

    changed kitty and nvim theme to everforest dark soft

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,16 @@
 return {
-  "vague2k/vague.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other plugins
-  config = function()
-    -- NOTE: you do not need to call setup if you don't want to.
-    require("vague").setup({
-      -- optional configuration here
-    })
-    vim.cmd("colorscheme vague")
-  end
+  {
+    "neanias/everforest-nvim",
+    priority = 1000,
+    lazy = false,
+    init = function()
+      vim.o.background = "dark"
+    end,
+    config = function()
+      require("everforest").setup({
+        background = "soft",
+      })
+      vim.cmd("colorscheme everforest")
+    end,
+  },
 }
-
-
