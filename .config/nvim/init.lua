998cb64e65ee045bb36d4c3cbe5f895c81b54b9f
shai <jmshailesh587@gmail.com>
2026-05-24 17:15:25 +0800

    added lazygit keybind through vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,6 +71,7 @@ vim.keymap.set('n', '<C-k>', ':m .-2<CR>==', { desc = 'Move line up' })
 vim.keymap.set('n', '<C-j>', ':m .+1<CR>==', { desc = 'Move line down' })
 vim.keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('n', '<leader>g', ':terminal lazygit<CR>', { desc = 'Lazygit' })
 
 -- ============================================================
 -- OIL
